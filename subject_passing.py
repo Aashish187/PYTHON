4a5ba03cc07b2eaaaf18ed2861f5475a0a6b378f
Aashish Singh Negi <aashishsinghnegi0408@gmail.com>
2025-02-03 14:10:25 +0530

    subject_passing.py

--- subject_passing.py
+++ subject_passing.py
@@ -3,8 +3,8 @@ sub1=int(input("Enter your marks: "))
 sub2=int(input("Enter your marks: "))
 sub3=int(input("Enter your marks: "))
 # check for total percentage
-a=((sub1+sub2+sub3)/300)
-if a >= 0.4 and sub1>=33 and sub2>=33 and sub3>=33:
+total_percentage=((sub1+sub2+sub3)/300)*100
+if total_percentage >= 40 and sub1>=33 and sub2>=33 and sub3>=33:
     print("You are passed.")
 else :
     print("You are failed.")  
