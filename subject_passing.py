00d74fcadfcf565c62f9f18ae5d2132e297cc569
Aashish Singh Negi <aashishsinghnegi0408@gmail.com>
2025-02-03 14:12:30 +0530

    subject_passing.py

--- subject_passing.py
+++ subject_passing.py
@@ -5,7 +5,7 @@ sub3=int(input("Enter your marks: "))
 # check for total percentage
 total_percentage=((sub1+sub2+sub3)/300)*100
 if total_percentage >= 40 and sub1>=33 and sub2>=33 and sub3>=33:
-    print("You are passed.")
+    print("You are passed.",total_percentage)
 else :
-    print("You are failed.")  
+    print("You are failed.",total_percentage)  
 
