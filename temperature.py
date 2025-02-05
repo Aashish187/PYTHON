9cfc7eeeca2cd17d864b67306f99c21c7ff5e1f3
Aashish Singh Negi <aashishsinghnegi0408@gmail.com>
2025-02-05 14:42:08 +0530

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,5 +1,5 @@
-def conversion(temp_in_c):
+def conversion():
     temp_in_c=float(input("Enter the Temperature in °C: "))
     fahrenheit_temp= (temp_in_c*(9/5))+32
     print(f"Temperature in Fahrenheit is: {round(fahrenheit_temp,2)} °F")
-conversion(34)
+conversion()
