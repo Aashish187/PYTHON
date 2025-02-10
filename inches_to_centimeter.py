a78fc2b4310b26923157a407b5f9cf4f55de775d
Aashish Singh Negi <aashishsinghnegi0408@gmail.com>
2025-02-10 11:04:45 +0530

    Add files via upload

---
+++ inches_to_centimeter.py
@@ -0,0 +1,6 @@
+# COVERSION OF LENGTH FROM INCHES INTO CENTIMETER
+def length():
+    inches=float(input("Enter the Length in inches: "))
+    centimeter= inches*2.54
+    print(f"Length in centimeter is: {centimeter} cm")
+length()
